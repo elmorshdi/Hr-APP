7b8442fbe168741b67cbfc04990cd5feaa6acadf
mostafa <elmorshdi53@gmail.com>
2020-12-06 03:07:35 +0200

    Hr app

--- app/src/main/java/com/elmorshdi/hr/StatusFragment.java
+++ app/src/main/java/com/elmorshdi/hr/StatusFragment.java
@@ -1,18 +1,35 @@
 package com.elmorshdi.hr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 
-public class StatusFragment extends Fragment {
+public class StatusFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        View view = inflater.inflate(R.layout.fragment_status, container, false);
+        CardView cardView = view.findViewById(R.id.card);
+        cardView.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.card:
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
